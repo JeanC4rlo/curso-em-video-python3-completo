07624eb7a918a158248207bcd977a46adbfd7929
Jean Carlo <125710389+JeanC4rlo@users.noreply.github.com>
2023-03-31 16:52:38 -0300

    Desafio 001 - Mensagem de Boas-Vindas
    Adição do Desafio 001 que propõe a criação de um script capaz de enviar uma mensagem de boas-vindas.

---
+++ Módulo 1 - Fundamentos/Exercícios/Aula 4/Desafio 001.py
@@ -0,0 +1,8 @@
+"""
+Desafio 001
+
+ENUNCIADO: Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+de acordo com o valor digitado.
+"""
+nome = input('Qual é o seu nome? ')
+print('Olá', nome, ', prazer em te conhecer!')
