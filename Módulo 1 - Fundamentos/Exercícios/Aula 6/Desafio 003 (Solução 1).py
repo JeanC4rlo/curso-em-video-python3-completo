f921dfc1482df30941c2832aaa56c877eba23f31
Jean Carlo <125710389+JeanC4rlo@users.noreply.github.com>
2023-03-31 17:00:15 -0300

    Desafio 003 - Soma entre Dois Números
    Adição do Desafio 003 que propõe a criação de um script capaz de realizar a soma entre dois números da classe "int".
    
    OBS.: Foi feito o envio de duas soluções.

---
+++ Módulo 1 - Fundamentos/Exercícios/Aula 6/Desafio 003 (Solução 1).py
@@ -0,0 +1,9 @@
+"""
+Desafio 003 (Solução 1)
+
+ENUNCIADO: Crie um programa que leia dois números e ostre a soma entre eles.
+"""
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+soma = num1 + num2
+print(f'\nA soma entre {num1} e {num2} é {soma}.')
---
+++ Módulo 1 - Fundamentos/Exercícios/Aula 6/Desafio 003 (Solução 2).py
@@ -0,0 +1,9 @@
+"""
+Desafio 003 (Solução 2)
+
+ENUNCIADO: Crie um programa que leia dois números e mostre a soma entre eles.
+"""
+num1 = int(input('Primeiro número = '))
+num2 = int(input('Segundo número = '))
+soma = num1 + num2
+print('O resultado da soma é', soma)
