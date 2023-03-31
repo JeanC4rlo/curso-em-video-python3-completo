117cc1090e0becfc50ca8c19e1d63ec3f62e35f4
Jean Carlo <125710389+JeanC4rlo@users.noreply.github.com>
2023-03-31 19:40:56 -0300

    Desafio 007 - Média Aritmética
    Adição do Desafio 007 que propõe a criação de um script capaz de ler duas notas de um aluno e calcular sua média.

---
+++ Módulo 1 - Fundamentos/Exercícios/Aula 7/Desafio 007.py
@@ -0,0 +1,9 @@
+"""
+Desafio 007
+
+ENUNCIADO: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média.
+"""
+nota1 = float(input('Insira a primeira nota: '))
+nota2 = float(input('Insira a segunda nota: '))
+media = (nota1 + nota2) / 2
+print(f'A sua média é {media:.1f}')
