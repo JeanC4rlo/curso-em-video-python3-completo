e3da90e3cebd234aab9bc162dcc6cd8e30dd8683
Jean Carlo <125710389+JeanC4rlo@users.noreply.github.com>
2023-03-31 19:38:16 -0300

    Desafio 006 - Dobro, Triplo e Raiz Quadrada
    Adição do Desafio 006 que propõe a criação de um script capaz de ler um número enviado pelo terminal e informar seu dobro, triplo e raiz quadrada.

---
+++ Módulo 1 - Fundamentos/Exercícios/Aula 7/Desafio 006.py
@@ -0,0 +1,7 @@
+"""
+Desafio 006
+
+ENUNCIADO: Crie um algoritimo que leia um número e mostre seu dobrro, triplo e raiz quadrada.
+"""
+num = float(input('Insira um número: '))
+print(f'O dobro, o triplo e a raiz quadrada de {num} são: {num*2}, {num*3} e {num**(1/2)}, respectivamente.')
