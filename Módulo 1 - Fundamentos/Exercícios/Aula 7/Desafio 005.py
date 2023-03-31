3eb86d4332bbb2c28230f6b4cef0224649d94cef
Jean Carlo <125710389+JeanC4rlo@users.noreply.github.com>
2023-03-31 17:23:12 -0300

    Desafio 005 - Antecessor e Sucessor
    Adição do Desafio 005 que propõe a criação de um script capaz de ler um número enviado pelo terminal e informar seu antecessor e sucessor.

---
+++ Módulo 1 - Fundamentos/Exercícios/Aula 7/Desafio 005.py
@@ -0,0 +1,7 @@
+"""
+Desafio 005
+
+ENUNCIADO: Faça um programa que leia um número inteiro e mostre seu sucessor e seu antecessor.
+"""
+num = int(input('Insira um número: '))
+print(f'O antecessor e o sucessor de {num} são: {num-1} e {num+1}, respecticvamente.')
