e9d2c65f9025bc4447c22023c0770d4666bb7737
Jean Carlo <125710389+JeanC4rlo@users.noreply.github.com>
2023-03-31 19:51:52 -0300

    Desafio 009 - Tabuada
    Adição do Desafio 009 que propõe a criação de um script capaz de ler um valor e mostrar sua tabuada no terminal.
    
    OBS.: Foi feito o envio de duas soluções.

---
+++ Módulo 1 - Fundamentos/Exercícios/Aula 7/Desafio 009 (Solução 1).py
@@ -0,0 +1,27 @@
+"""
+Desafio 009 (Solução 1)
+
+ENUNCIADO: Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+"""
+num = int(input('Insira um número: '))
+res1 = num * 1
+res2 = num * 2
+res3 = num * 3
+res4 = num * 4
+res5 = num * 5
+res6 = num * 6
+res7 = num * 7
+res8 = num * 8
+res9 = num * 9
+res0 = num * 10
+print(f'\n{num} x 0 = 0'
+      f'\n{num} x 1 = {res1}'
+      f'\n{num} x 2 = {res2}'
+      f'\n{num} x 3 = {res3}'
+      f'\n{num} x 4 = {res4}'
+      f'\n{num} x 5 = {res5}'
+      f'\n{num} x 6 = {res6}'
+      f'\n{num} x 7 = {res7}'
+      f'\n{num} x 8 = {res8}'
+      f'\n{num} x 9 = {res9}'
+      f'\n{num} x 10 = {res0}')
---
+++ Módulo 1 - Fundamentos/Exercícios/Aula 7/Desafio 009 (Solução 2).py
@@ -0,0 +1,17 @@
+"""
+Desafio 009 (Solução 2)
+
+ENUNCIADO: Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+"""
+num = int(input('Insira um número:'))
+print(f'\n{num} x 0 = {num*0}'
+      f'\n{num} x 1 = {num * 1}'
+      f'\n{num} x 2 = {num * 2}'
+      f'\n{num} x 3 = {num * 3}'
+      f'\n{num} x 4 = {num * 4}'
+      f'\n{num} x 5 = {num * 5}'
+      f'\n{num} x 6 = {num * 6}'
+      f'\n{num} x 7 = {num * 7}'
+      f'\n{num} x 8 = {num * 8}'
+      f'\n{num} x 9 = {num * 9}'
+      f'\n{num} x 10 = {num * 10}')
